9d1175ff61f611e3fd355412e974a3bda6392df6
Kavit Trivedi <knt1993@gmail.com>
2022-06-14 15:32:09 +0530

    - Create Requests

---
+++ app/Http/Requests/LinkRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LinkRequest extends FormRequest
+{
+    protected $redirect = '/';
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'             => ['required'],
+            'link'              => ['required', 'url'],
+            'open_in_new_tab'   => ['nullable', 'boolean']
+        ];
+    }
+}
